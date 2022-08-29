c2b6f541fe2d40abe459eafa049c4bb71e073ca1
venkata-hari <xprogrammer.me@gmail.com>
2022-08-29 10:19:45 +0530

    My first commit

--- src/components/Home/filter.js
+++ src/components/Home/filter.js
@@ -3,11 +3,11 @@ import React,{Fragment} from 'react';
 function Homefilter({f1}) {
     return (
         <Fragment>
-      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("welcomedrink")}>welcomedrink</button>
-      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("soup")}>soup</button>
-      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("salad")}>salad</button>
-      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("starters")}>starters</button>
-      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("maincourses")}>maincourses</button>
+      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("welcomedrink")}>Welcomedrink</button>
+      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("soup")}>Soup</button>
+      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("salad")}>Salad</button>
+      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("starters")}>Starters</button>
+      <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("maincourses")}>Maincourses</button>
       <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("dessert")}>dessert</button>
       <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("Chinese")}>Chinese</button>
       <button className="btn btn-dark p-3 border border-white" onClick={()=>f1("mexican")}>mexican</button>
